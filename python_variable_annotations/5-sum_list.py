738fb188a5ad8dd58e7bae55777c805c4d32d952
tmlgde <tom.lagarde33@outlook.fr>
2025-07-30 13:33:54 +0200

    return sum of a list

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-"""sum of list into a float"""
+"""Module that defines a function to sum a list of floats."""
 
 from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
-    """return this list"""
-
+def sum_list(input_list: List[float]) -> float:
+     """Return the sum of a list of floats."""
     return sum(input_list)
+
