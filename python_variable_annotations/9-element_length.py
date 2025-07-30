ca60bc09b1498d9e8c4b888caa4e16f7ae854bdf
tmlgde <tom.lagarde33@outlook.fr>
2025-07-30 14:08:43 +0200

    using iterable, sequence, list and tuple

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Module that defines a function which returns the length of iterable elements."""
+
+from typing import Iterable, Sequence, List, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """Return a list of tuples (element, length) for each sequence in lst."""
+    return [(i, len(i)) for i in lst]
