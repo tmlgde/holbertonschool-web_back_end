c33558426ab29540756e83b35222745fd09e66a5
tmlgde <tom.lagarde33@outlook.fr>
2025-07-30 14:25:43 +0200

    using await for a random number

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python3
-"""Module that defines an asynchronous coroutine to wait for a random delay."""
-
+"""Module defines an asynchronous coroutine to wait for a random delay."""
 
 import asyncio
 import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    """Wait for a random delay between 0 and max_delay seconds (float), then return it."""
+    """
+    Wait for a random delay between 0 and max_delay seconds and return delay
+
+    Args:
+        max_delay (int): maximum number of seconds to wait (default: 10)
+
+    Returns:
+        float: the random delay that was waited
+    """
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
