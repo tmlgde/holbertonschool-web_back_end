b68d9963ae57fc0983bef4349880156b9b42efca
tmlgde <tom.lagarde33@outlook.fr>
2025-07-30 16:25:21 +0200

    using wait-random for haviing a list of number

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-"""Module that defines a coroutine to run multiple wait_random concurrently."""
-
+"""
+Module 1-concurrent_coroutines.py
+"""
 import asyncio
 from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
@@ -8,18 +9,14 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """
-    Spawn wait_random n times with the given max_delay and return list of delays.
-
-    Args:
-        n (int): number of times to run wait_random
-        max_delay (int): maximum delay in seconds
-
-    Returns:
-        List[float]: list of delay times in ascending order
+    Spawn wait_random n times with the specified max_delay
+    Returns the list of all the delays (float values)
     """
-    delays: List[float] = []
-    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
-    for task in asyncio.as_completed(tasks):
-        delay = await task
-        delays.append(delay)
-    return delays
+
+    coroutines = [wait_random(max_delay) for i in range(n)]
+
+    delays_list = []
+    for completed in asyncio.as_completed(coroutines):
+        delay = await completed
+        delays_list.append(delay)
+    return delays_list
