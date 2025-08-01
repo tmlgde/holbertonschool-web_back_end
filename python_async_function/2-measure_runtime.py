14861fa0d9d9366f47587284d1eb54a7dd488d7c
tmlgde <tom.lagarde33@outlook.fr>
2025-08-01 10:25:42 +0200

    calcul time between two lines of code

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+"""Taking module from task 1"""
+
 import asyncio
 import time
 
@@ -7,6 +9,8 @@ wait_n = __import__("1-concurrent_coroutines").wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
+    """calcul time between two lines of code"""
+    
     start = time.perf_counter()
     asyncio.run(wait_n(n, max_delay))
     end = time.perf_counter()
