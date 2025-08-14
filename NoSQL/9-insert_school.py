9e7055efefde586116d8e72c93d06a8019c05e1c
tmlgde <tom.lagarde33@outlook.fr>
2025-08-14 11:33:45 +0200

    insert a new document in a MongoDB collection

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Insert a new document in a MongoDB collection"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """Fuction isert a new document and return his id"""
+
+    if mongo_collection is None:
+        return None
+    res = mongo_collection.insert_one(dict(kwargs))
+    return res.inserted_id
+
+
+if __name__ == "__main__":
+    pass
