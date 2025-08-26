65ff6f3b6b47ae564b7a78b45c106181ee7a857b
tmlgde <tom.lagarde33@outlook.fr>
2025-08-26 13:24:09 +0200

    using slice for "data-cleaning"

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,13 @@
+export default function cleanSet(set, startString) {
+  if (!startString || typeof startString !== 'string') return '';
+
+  const result = [];
+
+  set.forEach((value) => {
+    if (typeof value === 'string' && value.startsWith(startString)) {
+      result.push(value.slice(startString.length));
+    }
+  });
+
+  return result.join('-');
+}
