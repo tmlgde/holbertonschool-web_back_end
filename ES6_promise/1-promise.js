a94e28d395d0c1c8af6a235266b5f655ec399cc8
tmlgde <tom.lagarde33@outlook.fr>
2025-08-28 13:57:30 +0200

    false or true promise

---
+++ ES6_promise/1-promise.js
@@ -0,0 +1,12 @@
+export default function getFUllResponseFormat(success) {
+    return new Promise((resolve, reject) => {
+        if (success) {
+            resolve({
+                status:200,
+                body: 'Success',
+            });
+        } else {
+            reject(new Error('The fake API is not working currently'));
+        }
+    });
+}
