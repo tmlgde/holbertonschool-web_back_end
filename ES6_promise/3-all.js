7ecda058bcb140c358b7c0ff8f4e4471757e6499
tmlgde <tom.lagarde33@outlook.fr>
2025-08-28 14:23:52 +0200

    return many promises

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,3 +1,6 @@
+// 3-all.js
+import { uploadPhoto, createUser } from './utils.js';
+
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then(([photo, user]) => {
