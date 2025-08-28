1d0ebb46b6008fc5f5507cd4f04b8b3a6eac107c
tmlgde <tom.lagarde33@outlook.fr>
2025-08-28 14:39:12 +0200

    use many promises if succes or reject

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,14 @@
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((results) =>
+    results.map((r) => ({
+      status: r.status,
+      value: r.status === 'fulfilled' ? r.value : r.reason,
+    }))
+  );
+}
