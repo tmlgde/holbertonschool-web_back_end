e428d3b7690187964b4fe7714cefda32f72d2ba6
tmlgde <tom.lagarde33@outlook.fr>
2025-09-03 14:25:50 +0200

    try an other code

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -3,21 +3,24 @@ const countStudents = require('./3-read_file_async');
 
 const app = http.createServer((req, res) => {
   if (req.url === '/') {
-    res.writeHead(200, { 'Content-type': 'text/plain' });
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Holberton School!');
-    
   } else if (req.url === '/students') {
-    res.writeHead(200, { 'Content-type': 'text/plain' });
-    countStudents(process.argv[2]) // récupère le chemin du CSV
-      .then((texte) => {
-        res.end(`This is the list of our students\n${texte}`);
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('This is the list of our students\n');
+    
+    countStudents(process.argv[2])
+      .then((studentsData) => {
+        res.end(studentsData);
       })
-      .catch(() => {
+      .catch((error) => {
         res.end('Cannot load the database');
       });
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found');
   }
 });
 
 app.listen(1245);
-
 module.exports = app;
