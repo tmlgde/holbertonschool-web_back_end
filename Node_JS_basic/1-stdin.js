e7b9005e0e70bc19d38fd44d591d5e4033e563ea
tmlgde <tom.lagarde33@outlook.fr>
2025-09-03 11:23:17 +0200

    using process.stdin

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,11 @@
+process.stdin.setEncoding('utf-8'); // for real string
+console.log('Welcome to Holberton School, what is your name?');
+
+process.stdin.on('data', (input) => {
+    const name = input.trim(); // for delete /n
+    console.log(`Your name is: ${name}`);
+});
+
+process.stdin.on('end', () => {
+    console.log('This important software is now closing');
+});
