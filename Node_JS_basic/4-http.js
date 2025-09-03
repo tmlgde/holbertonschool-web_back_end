0ebcaace297976af303b3a3a6fc9b8ef3bd1392a
tmlgde <tom.lagarde33@outlook.fr>
2025-09-03 13:34:35 +0200

    using writeHead instead of res.Head

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -2,9 +2,10 @@ const http = require('http');
 
 const app = http.createServer((req, res) => {
     res.statusCode = 200;
-    res.setHeader('Content-type', 'text/plain');
-    res.end('Hello Holberton School!')
+    res.writeHead(200, { 'Content-type': 'text/plain' });
+    res.end('Hello Holberton School!');
 });
 
 app.listen(1245);
+
 module.exports = app;
