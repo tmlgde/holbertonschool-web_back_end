cb8c5fc2b5382725d6a7991e37a1d09594b1fb4d
tmlgde <tom.lagarde33@outlook.fr>
2025-09-04 11:18:26 +0200

    try with the content of file 3

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const countStudents = require('./3-read_file_async');
+const fs = require('fs');
 
 const app = express();
 
@@ -8,28 +8,42 @@ app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
-// Route pour /students
+// Route pour /students - logique directe sans import
 app.get('/students', (req, res) => {
-  res.set('Content-Type', 'text/plain');
   let responseText = 'This is the list of our students\n';
   
-  // Récupérer le chemin du fichier database depuis les arguments
   const database = process.argv[2];
   
   if (!database) {
     return res.send(responseText + 'Cannot load the database');
   }
   
-  // Utiliser countStudents pour récupérer les données
-  countStudents(database)
-    .then((studentsData) => {
-      responseText += studentsData;
-      res.send(responseText);
-    })
-    .catch((error) => {
-      responseText += 'Cannot load the database';
-      res.send(responseText);
-    });
+  // Logique directe inspirée de 3-read_file_async.js
+  fs.readFile(database, 'utf8', (err, data) => {
+    if (err) {
+      return res.send(responseText + 'Cannot load the database');
+    }
+
+    const lines = data.split(/\r?\n/);
+    const students = lines.slice(1).filter((line) => line.trim() !== '');
+    
+    responseText += `Number of students: ${students.length}\n`;
+
+    const groups = {};
+    for (const line of students) {
+      const cols = line.split(',');
+      const firstname = cols[0].trim();
+      const field = cols[cols.length - 1].trim();
+      if (!groups[field]) groups[field] = [];
+      groups[field].push(firstname);
+    }
+    
+    for (const [field, list] of Object.entries(groups)) {
+      responseText += `Number of students in ${field}: ${list.length}. List: ${list.join(', ')}\n`;
+    }
+    
+    res.send(responseText.trim());
+  });
 });
 
 // Démarrer le serveur sur le port 1245
